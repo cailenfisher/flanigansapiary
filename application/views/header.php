183ead1fdf9ce2e72506cf6a96e3bf4ece304679
tmorehead <trmorehead@gmail.com>
2013-08-18 22:36:54 -0400

    Header and navigation links. Created .php docs for each link.

--- application/views/header.php
+++ application/views/header.php
@@ -1,1 +1,32 @@
-header
+<html>
+	<head>
+		<title>
+		Flannigan's Apiary
+		</title>
+		<!--Jquery links-->
+		
+		<!--end Jquery-->
+	</head>
+	<body>
+		<div id="logowrap"><a href="/"><img src="" /></a></div>
+			<div id="navigation">
+				<span class="link">
+				<a href="flanigansapiary/index.php/welcome/index/home">Home</a>
+				</span>
+				
+				<span class="link">
+				<a href="flanigansapiary/index.php/welcome/index/how-to">How to/Info</a>
+				</span>
+				
+				<span class="link">
+				<a href="/index.php/welcome/index/ask-bee-man">Ask The Bee Man</a>
+				</span>
+				
+				<span class="link">
+				<a href="/index.php/welcome/index/store">Store</a>
+				</span>
+			</div>
+
+
+	</body>
+</html>
