3bfcab5e39a574f37a2571fa0bb176425ab2f7ff
tmorehead <trmorehead@gmail.com>
2013-08-18 23:46:46 -0400

    header styling / css link

--- application/views/header.php
+++ application/views/header.php
@@ -7,29 +7,41 @@
 		<!--Jquery links-->
 		
 		<!--end Jquery-->
+		
+		<style>
+			#logowrap {
+				width: 10%;
+				float: left;
+			}
+			
+			#bigwrap {
+				float: right;
+			}
+		</style>
 	</head>
 	<body>
 		<div id="logowrap"><a href="home.php"><img src="" />LOGO</a></div>
-			<!--Navigation Block-->
-			<div id="navigation">
+			<div id="bigwrap">
 				<div id="sloganwrapper">Flanigan's Apiary Slogan</div>
-				
-				<span class="link">
-				<a href="home.php">Home</a>
-				</span>
-				
-				<span class="link">
-				<a href="how-to.php">How to/Info</a>
-				</span>
-				
-				<span class="link">
-				<a href="ask-bee-man.php">Ask The Bee Man</a>
-				</span>
-				
-				<span class="link">
-				<a href="store.php">Store</a>
-				</span>
+				<!--Navigation Block-->
+				<div id="navigation">
+					<span class="link">
+					<a href="home.php">Home</a>
+					</span>
+					
+					<span class="link">
+					<a href="how-to.php">How to/Info</a>
+					</span>
+					
+					<span class="link">
+					<a href="ask-bee-man.php">Ask The Bee Man</a>
+					</span>
+					
+					<span class="link">
+					<a href="store.php">Store</a>
+					</span>
+				</div>
+				<!--End Navigation-->
 			</div>
-			<!--End Navigation-->
 	</body>
 </html>
