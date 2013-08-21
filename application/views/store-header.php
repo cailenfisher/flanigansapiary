e1deaaa95d22edcf0167ce8a714edb847e798a12
tmorehead <trmorehead@gmail.com>
2013-08-20 23:07:00 -0400

    store-header id changes

--- application/views/store-header.php
+++ application/views/store-header.php
@@ -3,11 +3,11 @@
 		<title>
 		Flannigan's Apiary
 		</title>
-<<<<<<< HEAD
-	<link rel="stylesheet" type="text/css" href="/flanigansapiary/css/styles2.css" />
-	<!--Jquery Links-->
-	
-	<!--End Jquery links-->
+		<link rel="stylesheet" type="text/css" href="/flanigansapiary/css/styles.css" />
+		<link rel="stylesheet" type="text/css" href="/flanigansapiary/css/store-styles.css" />
+		<!--Jquery links-->
+		
+		<!--end Jquery-->
 	</head>
 <body>
 	<div id="store-logo-slogan-navigation-wrap">
@@ -36,38 +36,3 @@
 		</div>
 	</div>
 	<div id="header2-photo"></div>
-=======
-		<link rel="stylesheet" type="text/css" href="/flanigansapiary/css/styles.css" />
-		<link rel="stylesheet" type="text/css" href="/flanigansapiary/css/store-styles.css" />
-		<!--Jquery links-->
-		
-		<!--end Jquery-->
-		
-	</head>
-	<body>
-		<div id="logo-slogan-navigation-wrap">
-			<a href="home.php"><div id="logowrap"><img src="" />LOGO</div></a>
-				<div id="slogan-navigation-wrap">
-					<div id="sloganwrap"><h1>Flanigan's Apiary Slogan Flanigan's Apiary Slogan</h1></div>
-					<!--Navigation Block-->
-					<div id="navigation">
-						<span class="link">
-						<a href="home.php">Home</a>
-						</span>
-						
-						<span class="link">
-						<a href="how-to.php">How to/Info</a>
-						</span>
-						
-						<span class="link">
-						<a href="ask-bee-man.php">Ask The Bee Man</a>
-						</span>
-						
-						<span class="link">
-						<a href="store.php">Store</a>
-						</span>
-					</div>
-					<!--End Navigation-->
-				</div>
-		</div>
->>>>>>> 359931f5b159fc918e13a472e6be1577efddd2bc
