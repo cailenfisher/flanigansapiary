359931f5b159fc918e13a472e6be1577efddd2bc
Cailen Fisher <cailen.fisher@gmail.com>
2013-08-20 22:30:46 -0400

    store header and footer files

---
+++ application/views/store-header.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<title>
+		Flannigan's Apiary
+		</title>
+		<link rel="stylesheet" type="text/css" href="/flanigansapiary/css/styles.css" />
+		<link rel="stylesheet" type="text/css" href="/flanigansapiary/css/store-styles.css" />
+		<!--Jquery links-->
+		
+		<!--end Jquery-->
+		
+	</head>
+	<body>
+		<div id="logo-slogan-navigation-wrap">
+			<a href="home.php"><div id="logowrap"><img src="" />LOGO</div></a>
+				<div id="slogan-navigation-wrap">
+					<div id="sloganwrap"><h1>Flanigan's Apiary Slogan Flanigan's Apiary Slogan</h1></div>
+					<!--Navigation Block-->
+					<div id="navigation">
+						<span class="link">
+						<a href="home.php">Home</a>
+						</span>
+						
+						<span class="link">
+						<a href="how-to.php">How to/Info</a>
+						</span>
+						
+						<span class="link">
+						<a href="ask-bee-man.php">Ask The Bee Man</a>
+						</span>
+						
+						<span class="link">
+						<a href="store.php">Store</a>
+						</span>
+					</div>
+					<!--End Navigation-->
+				</div>
+		</div>
